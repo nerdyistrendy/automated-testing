64633a482b195a159f0034b8f7323aa26ce2bb4f
Li Dai <li.lea.dai@gmail.com>
2020-09-29 11:29:20 -0700

    used before to run "Arrange" code before each test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,39 +5,37 @@ describe Deck do
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
+  before do
+    @deck = Deck.new
+  end
 
   describe "You can create a deck instance" do
 
     it "Can be created" do
-      deck = Deck.new
-        expect(deck).must_be_instance_of Deck
+        expect(@deck).must_be_instance_of Deck
       end
   end
-
 
   describe "You can draw a card" do
 
     it "draws a card and return the card" do
-      deck = Deck.new
 
-      expect(deck.draw).must_be_instance_of Card
+      expect(@deck.draw).must_be_instance_of Card
 
     end
 
     it "the card is gone" do
-      deck = Deck.new
-      before_length = deck.cards.length
+      before_length = @deck.cards.length
 
-      expect(deck.cards.include?(deck.draw)).must_equal false
-      expect(deck.cards.length).must_equal (before_length - 1)
+      expect(@deck.cards.include?(@deck.draw)).must_equal false
+      expect(@deck.cards.length).must_equal (before_length - 1)
     end
 
     it "can't draw when no card left" do
-      deck = Deck.new
       52.times do
-        deck.draw
+        @deck.draw
       end
-      expect(deck.draw).must_be_nil
+      expect(@deck.draw).must_be_nil
     end
 
   end
@@ -45,18 +43,16 @@ describe Deck do
   describe "You can count the numbers of cards" do
 
     it "count the deck" do
-      deck = Deck.new
-      deck.cards.delete_if {|card| card.value == 4}
+      @deck.cards.delete_if {|card| card.value == 4}
 
-      expect(deck.cards.length).must_equal 48
+      expect(@deck.cards.length).must_equal 48
     end
   end
 
   describe "shuffle" do
 
     it "responds to shuffle" do
-      deck = Deck.new
-      expect(deck).must_respond_to :shuffle
+      expect(@deck).must_respond_to :shuffle
     end
   end
 end
